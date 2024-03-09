6ba639dcb0c8b465cd2b476a26940e0514e14cf7
jankat <cankatguven@gmail.com>
2024-03-09 12:24:30 +0300

    initial

--- app/layout.js
+++ app/layout.js
@@ -4,9 +4,9 @@ import Layout from "@/components/Layout";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "design4theplanet",
+  title: "Zeliş'in Mutfağı",
   description:
-    "We value our planet and are dedicated to contributing to its well-being by upcycling waste materials and turning them into one of a kind gadgets, utensils and art objects.",
+    "Ev yemekleri",
 };
 
 export default function RootLayout({ children }) {
