ed2bc51774d63d95a0b19fd801f98b8b7b67b4f6
cankat <cankatguven@gmail.com>
2024-03-21 02:28:44 +0300

    new photos add

--- components/introduction-title.js
+++ components/introduction-title.js
@@ -5,7 +5,7 @@ import { motion } from "framer-motion"
 
 const IntroductionTitle = ({ isRight, title }) => {
   const [ref, inView] = useInView({
-    threshold: 0.5,
+    threshold: 1,
     triggerOnce: true,
   });
   return (
