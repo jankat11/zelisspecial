0f2a36c2a8696ba3c3afbeb7d20635c83ac0dbbf
cankat <cankatguven@gmail.com>
2024-03-21 12:30:26 +0300

    new photos add

--- components/introduction-title.js
+++ components/introduction-title.js
@@ -5,21 +5,21 @@ import { useInView } from "react-intersection-observer";
 const IntroductionTitle = ({ isRight, title }) => {
   const [ref, inView] = useInView({
     threshold: 1,
-    triggerOnce: true,
+    triggerOnce: false,
   });
   return (
     <div>
       <div ref={ref} className={`w-full md:hidden  z-20 relative bottom-32`}>
         <p
           className={`home-images-title opacity-0 font-light ${
-            isRight ? "rounded-l-full ms-auto" : "rounded-r-full"
+            isRight ? "rounded-l-full border-r-0 ms-auto" : "rounded-r-full border-l-0"
           } ${inView && (isRight ? "tilt-in-bottom-1" : "tilt-in-bottom-2")}`}
         >
           {title}
         </p>
       </div>
       <div className={`w-full absolute hidden md:block  bottom-8`}>
-        <p className="home-images-title border-none font-normal">{title}</p>
+        <p className="home-images-title  font-normal">{title}</p>
       </div>
     </div>
   );
